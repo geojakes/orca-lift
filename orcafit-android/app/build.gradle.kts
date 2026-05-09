cce693f58877ea50b79ff1a9d614e48ce16a9208
George Jacob <georgejakes+github.com>
2026-05-09 11:57:38 -0400

    More updates

--- orcafit-android/app/build.gradle.kts
+++ orcafit-android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
 
-        buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8000\"")
+        buildConfigField("String", "API_BASE_URL", "\"http://192.168.4.35:8000\"")
     }
 
     buildTypes {
